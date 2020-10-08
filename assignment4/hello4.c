28297e3c5ed72d6d50907cf79753a09d7f1b89db
Nikita Tipule <nikitatipule@gmail.com>
2020-10-09 00:06:33 +0530

    Assignment 4 added

---
+++ assignment4/hello4.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+	float f = 2.1233;
+	return 0;
+}
---
+++ assignment4/hello5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+struct node{
+	int i;
+	char ch;
+};
+
+int main() {
+	struct node s1, s2;
+	s1.i = 1;
+	s1.ch = 'a';
+	s2.i = 2;
+	s2.ch = 'b';
+	return 0;	
+}
